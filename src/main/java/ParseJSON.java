c6e2393de22e999589dc80aaad0d3cb28e4c85ae
Edward <nechitoaiageorgeedward@gmail.com>
2021-02-05 14:36:17 +0200

    parse and send multiple generated files

--- src/main/java/ParseJSON.java
+++ src/main/java/ParseJSON.java
@@ -4,6 +4,7 @@ import ca.uhn.fhir.parser.StrictErrorHandler;
 import org.hl7.fhir.r4.model.*;
 
 import java.awt.datatransfer.DataFlavor;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -20,16 +21,23 @@ public class ParseJSON {
         IParser parser2 = ctx.newJsonParser();
         // Parse it
         //Patient parsed = parser.parseResource(Patient.class, input);
-        Bundle bundle = parser.parseResource(Bundle.class,new FileReader("data.json"));
-        for(Bundle.BundleEntryComponent p : bundle.getEntry()){
-            Resource rsc = p.getResource();
-            String rawData = parser.encodeResourceToString(rsc);
-            String url = p.getChildByName("request").getValues().get(0).getChildByName("url").getValues().get(0).toString();
-            url = url.substring(8,url.length()-1);
-            Send sender = new Send();
-            sender.SendResource(url, rawData, accesstoken);
+        String outputPath = System.getProperty("user.dir").concat("/output/fhir/");
+        File dir = new File( outputPath);
+        System.out.println(dir.getName());
+        File[] directoryListing = dir.listFiles();
+        for(File f: directoryListing) {
+            System.out.println("Current File: "+ f.getName());
+            if(!f.getName().contains("json")) continue;
+            Bundle bundle = parser.parseResource(Bundle.class, new FileReader(outputPath.concat(f.getName())));
+            for (Bundle.BundleEntryComponent p : bundle.getEntry()) {
+                Resource rsc = p.getResource();
+                String rawData = parser.encodeResourceToString(rsc);
+                String url = p.getChildByName("request").getValues().get(0).getChildByName("url").getValues().get(0).toString();
+                url = url.substring(8, url.length() - 1);
+                Send sender = new Send();
+                sender.SendResource(url, rawData, accesstoken);
 
-        }
-
+            }
+      }
     }
 }
