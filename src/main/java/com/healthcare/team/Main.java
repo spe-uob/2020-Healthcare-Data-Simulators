c87f8c9797c17f2dafd2455f7af8f396f4c17361
vladbucur2000 <vladbcr@gmail.com>
2021-03-17 14:45:32 +0200

    test

--- src/main/java/com/healthcare/team/Main.java
+++ src/main/java/com/healthcare/team/Main.java
@@ -4,7 +4,7 @@ package com.healthcare.team;
 
 public class Main {
     public static void main(String[] args)  {
-        //UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        ///UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         System.out.println("vlad");
         GUIForm mainMenu = new GUIForm();
         mainMenu.setVisible(true);
