af1c7e8d943f524a14f4c0c410ae88229272a6c1
Ena <60359345+ennaena@users.noreply.github.com>
2021-05-13 02:18:26 +0100

    gui improvements

--- src/main/java/com/healthcare/team/GUI/GeneratorForm.java
+++ src/main/java/com/healthcare/team/GUI/GeneratorForm.java
@@ -1,16 +1,20 @@
 package com.healthcare.team.GUI;
 
 import com.healthcare.team.BashProcess.ConfigurationSynthea;
-
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Color;
+import javax.swing.BorderFactory;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+import java.awt.Dimension;
 
 public class GeneratorForm extends JFrame {
     private JPanel rootPanel;
     private JButton generateSyntheticDataButton;
     private JButton addHealthDataFlatButton;
-    private JButton backToMainMenuButton;
+    private JButton backToMenuButton;
     private JTextArea textArea1;
     private JTextField selectedRegion;
 
@@ -19,10 +23,20 @@ public class GeneratorForm extends JFrame {
         super("Healthcare Data Simulator");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         add(rootPanel);
+        rootPanel.setBackground( Color.decode("#97BAEC") );
+        rootPanel.setForeground( Color.decode("#2C43B8"));
+        generateSyntheticDataButton.setPreferredSize(new Dimension(245, 75));
+        addHealthDataFlatButton.setPreferredSize(new Dimension(245, 75));
+        generateSyntheticDataButton.setBorder(BorderFactory.createLineBorder(Color.decode("#6987D5"), 4, true));
+        addHealthDataFlatButton.setBorder(BorderFactory.createLineBorder(Color.decode("#445BC1"), 4, true));
+        Border borderCustom=BorderFactory.createBevelBorder(BevelBorder.RAISED, Color.decode("#445BC1"), Color.decode("#1727AE")); // top, bottom colours
+        backToMenuButton.setPreferredSize(new Dimension(80, 30));
+        backToMenuButton.setBorder(borderCustom);
+
         setTitle("Healthcare Data Simulator");
         setSize(500,600);
 
-        backToMainMenuButton.addActionListener(new ActionListener() {
+        backToMenuButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 setVisible(false);
