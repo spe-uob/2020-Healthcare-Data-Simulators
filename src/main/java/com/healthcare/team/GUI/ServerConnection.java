1e4748f6e29442df59f00e7910faf2137b2ec2ca
Ena <60359345+ennaena@users.noreply.github.com>
2021-05-13 02:14:46 +0100

    gui improvement

--- src/main/java/com/healthcare/team/GUI/ServerConnection.java
+++ src/main/java/com/healthcare/team/GUI/ServerConnection.java
@@ -3,10 +3,16 @@ package com.healthcare.team.GUI;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Color;
+import javax.swing.BorderFactory;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.Border;
+import java.awt.Dimension;
 
 public class ServerConnection extends JFrame {
 
     private JTextField username;
+    private JTextField password;
     private JTextField region;
     private JTextField clientID;
     private JPanel rootPanel;
@@ -15,24 +21,35 @@ public class ServerConnection extends JFrame {
     private JLabel usernameLabel;
     private JLabel passwordLabel;
     private JLabel clientIDLabel;
-    private JPasswordField password;
+
+
 
     public ServerConnection() {
         super("Healthcare Data Simulator");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         add(rootPanel);
+        rootPanel.setBackground( Color.decode("#97BAEC") );
+        Border borderCustom=BorderFactory.createBevelBorder(BevelBorder.RAISED, Color.decode("#445BC1"), Color.decode("#1727AE")); // top, bottom colours
+        nextButton.setPreferredSize(new Dimension(60, 30));
+        nextButton.setBorder(borderCustom);
+        Border usualBorder =BorderFactory.createLineBorder(Color.decode("#BBDFFA"), 4, true);
+        clientID.setBorder(usualBorder);
+        username.setBorder(usualBorder);
+        region.setBorder(usualBorder);
+        password.setBorder(usualBorder);
         setTitle("Healthcare Data Simulator");
         setSize(500,600);
+
         nextButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //OAuth oAuth = new OAuth(clientID.getText(), region.getText(), username.getText(), password.getText());
+               // OAuth oAuth = new OAuth(clientID.getText(), region.getText(), username.getText(), password.getText());
 
                 //if(Utils.isValidString(oAuth.generateToken())){
                     setVisible(false);
                     RegionSelection regionSelection = new RegionSelection();
                     regionSelection.setVisible(true);
-                  //  oAuth.sendToRabbitMQ();
+                    //oAuth.sendToRabbitMQ();
                 //}
             }
         });
